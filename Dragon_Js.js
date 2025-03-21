04f7a1130a0b80804df28376694eff26697b6090
GeekJulia <juliayaks148@gmail.com>
2025-03-21 17:38:54 +0100

    Added buttons javascript declaration and monster stats

--- Dragon_Js.js
+++ Dragon_Js.js
@@ -1,7 +1,9 @@
 let xp,currentWeaponIndex = 0;
 let health = 100;
 let gold = 50;
-let fighting,monsterHealth
+let fighting,monsterHealth;
 let inventory = ["stick"];
 
-let button1 = document.querySelector("#button1"); // prefixed with # because it is a id, prefix will not be added if it was a normal element.
+const button1 = document.querySelector("#button1"); // prefixed with # because it is a id, prefix will not be added if it was a normal element.
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
