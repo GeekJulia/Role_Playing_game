8d4152006e68456da8a1705993e1e69c524501ed
GeekJulia <juliayaks148@gmail.com>
2025-03-21 08:46:46 +0100

    Layout

---
+++ Dragon_Js.js
@@ -0,0 +1,7 @@
+let xp,currentWeaponIndex = 0;
+let health = 100;
+let gold = 50;
+let fighting,monsterHealth
+let inventory = ["stick"];
+
+let button1 = document.querySelector("#button1"); // prefixed with # because it is a id, prefix will not be added if it was a normal element.
