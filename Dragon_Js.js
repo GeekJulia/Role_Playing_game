8f84c553ae8259de3afcab73ebc26773a07f94e8
GeekJulia <juliayaks148@gmail.com>
2025-03-26 09:36:09 +0100

    Added other ids to javascript

--- Dragon_Js.js
+++ Dragon_Js.js
@@ -7,3 +7,12 @@ let inventory = ["stick"];
 const button1 = document.querySelector("#button1"); // prefixed with # because it is a id, prefix will not be added if it was a normal element.
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
