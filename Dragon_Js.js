701c2d03aa15bee69d6527903062405d9fb400f3
GeekJulia <juliayaks148@gmail.com>
2025-03-26 10:23:31 +0100

    New onclick for buttons

--- Dragon_Js.js
+++ Dragon_Js.js
@@ -17,7 +17,13 @@ const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
 function goStore(){
-    console.log("Going to store.");
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    
+    button1.onclick = "buyHealth";
+    button2.onclick = "buyWeapon";
+    button3.onclick = "goTown";
 }
 function goCave(){
     console.log("Going to cave.");
@@ -26,3 +32,6 @@ function fightDragon(){
     console.log("Fighting dragon.");
 }
 //initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
