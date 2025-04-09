883ff99c1e491fc7ed06b3d277ec4709e985ae89
GeekJulia <juliayaks148@gmail.com>
2025-04-09 15:16:02 +0100

    Completed the defeat function

--- Dragon_Js.js
+++ Dragon_Js.js
@@ -188,6 +188,9 @@ function dodge(){
 function defeatMonster(){
   gold += Math.floor(monsters[fighting].level * 6.7)
   xp += monsters[fighting].level
+  goldText.innerText = gold
+  xpText.innerText = xp
+  update(locations[4])
 }
 function dodge(){
   text.innerText = "You dodge the attack from the " + monsters[fighting].name + "."
