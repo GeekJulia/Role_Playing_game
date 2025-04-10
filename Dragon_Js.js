a876e21184dcbdf4bc47d9c57b00d74391038dec
GeekJulia <juliayaks148@gmail.com>
2025-04-10 13:54:13 +0100

    Added the getMonsterAttackValue

--- Dragon_Js.js
+++ Dragon_Js.js
@@ -198,8 +198,10 @@ function goFight(){
 function attack(){
   text.innerText = "The " + monsters[fighting].name + " attacks."
   text.innerText += " You attack it with your "+ weapons[currentWeaponIndex].name + "."
-  health -= monsters[fighting].level
-  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1
+  health -= getMonsterAttackValue(monsters[fighting].level);
+  if(isMonsterHit()){
+    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1
+  }
   healthText.innerText = health
   monsterHealthText.innerText = monsterHealth
   if(health <= 0){
@@ -214,6 +216,13 @@ function attack(){
     }
   }
 }
+
+function getMonsterAttackValue(level) {
+  const hit = (level * 5) - (Math.floor(Math.random() * xp));
+  console.log(hit)
+  return hit > 0 ? hit : 0
+}
+
 function dodge(){
   text.innerText = "You dodge the attack from the " + monsters[fighting].name + "."
 }
