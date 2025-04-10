c81b1a46fcc60551819501a1ba94dfe6edf762fe
GeekJulia <juliayaks148@gmail.com>
2025-04-10 12:29:40 +0100

    Added a resart function

--- Dragon_Js.js
+++ Dragon_Js.js
@@ -198,8 +198,9 @@ function attack(){
   }
 }
 function dodge(){
-
+  text.innerText = "You dodge the attack from the " + monsters[fighting].name + "."
 }
+
 function defeatMonster(){
   gold += Math.floor(monsters[fighting].level * 6.7)
   xp += monsters[fighting].level
@@ -207,6 +208,19 @@ function defeatMonster(){
   xpText.innerText = xp
   update(locations[4])
 }
-function dodge(){
-  text.innerText = "You dodge the attack from the " + monsters[fighting].name + "."
+
+function lose(){
+  update(locations[6])
+}
+
+function restart(){
+  xp = 0
+  health = 100
+  gold = 50
+  currentWeaponIndex = 0
+  inventory=["stick"]
+  goldText.innerText = gold
+  healthText.innerText = health
+  xpText.innerText = xp
+  goTown()
 }
